4613a3232501f4fb9e471d7183473ca500770984
yurih1 <yuri.silva15@live.com>
2021-04-10 13:57:08 -0300

    Att  código

--- CalculadoraIMC/src/java/br/uninove/imc/Imc.java
+++ CalculadoraIMC/src/java/br/uninove/imc/Imc.java
@@ -38,9 +38,25 @@ public class Imc {
         this.classificacao = classificacao;
     }
     
-    public float calculaIMC(){
+    public float calculaIMC() {
         imc = peso / (altura * altura);
+        geraClassificacao();
         return imc;
     }
     
+    public void geraClassificacao() {
+        if(imc <= 18.5) {
+            classificacao = "Abaixo do peso";
+        } else if(imc >= 18.6 && imc <= 24.9) {
+            classificacao = "Peso ideal";
+        } else if(imc >= 25 && imc <= 29.9) {
+            classificacao = "Sobrepeso";
+        } else if(imc >= 30 && imc <= 34.9) {
+            classificacao = "Obesidade grau I";
+        } else if(imc >= 35 && imc <= 39.9) {
+            classificacao = "Obesidade grau II";
+        } else if(imc >= 40) {
+            classificacao = "Obesidade grau III";
+        }
+    }
 }
