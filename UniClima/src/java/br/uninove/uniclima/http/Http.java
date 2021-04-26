f9038fe2de1124d99f01ff24a3ba204074db6096
yurih1 <yuri.silva15@live.com>
2021-04-26 20:36:54 -0300

    API de clima

---
+++ UniClima/src/java/br/uninove/uniclima/http/Http.java
@@ -0,0 +1,61 @@
+package br.uninove.uniclima.http;
+
+import br.uninove.uniclima.tempo.Tempo;
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+
+public class Http {
+    
+    private static String readAll(Reader rd)throws IOException{
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while((cp = rd.read())!= -1){
+            sb.append((char) cp);
+        }
+        return sb.toString();
+    }
+    
+    public static Tempo getClima(String cidade) {
+        try {
+            String url = "http://api.openweathermap.org/data/2.5/weather";
+            String charset = StandardCharsets.UTF_8.name();
+            String appid = "f14e503991ae7a37006cc8c592a24ce8"; 
+            String units = "metric";
+            String lang = "pt_br";
+            
+            String query = String.format("q=%s&appid=%s&units=%s&lang=%s",
+                       URLEncoder.encode(cidade, charset),
+                       URLEncoder.encode(appid, charset),
+                       URLEncoder.encode(units, charset),
+                       URLEncoder.encode(lang, charset));
+            
+            URLConnection conn = new URL(url + "?" + query).openConnection();
+            
+            conn.setRequestProperty("Accept-Charset", charset);
+            conn.setDoOutput(true);
+            conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=" + charset);
+            Tempo t;
+            try(InputStream resp = conn.getInputStream()) {
+                BufferedReader buffer = new BufferedReader(new InputStreamReader(resp, Charset.forName("UTF-8")));
+                String json = readAll(buffer);
+                Gson gson = new Gson();
+                t = gson.fromJson(json, Tempo.class);
+            }
+           return t;
+        } catch(JsonSyntaxException | IOException e) {
+            
+        }
+        return null;
+    }
+}
